1eb8ba1e6408e5e7f420df2f9010522fe8859f18
Matheesha Dissanayake <SA22524904@my.sliit.lk>
2025-07-26 12:47:01 +0530

    create test api routes

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './db/connectDB.js';
+import userRoutes from './routes/user.js';
 
 dotenv.config();
 
@@ -12,3 +13,5 @@ app.listen(PORT, () => {
   connectDB()
   console.log(`Server is running on port ${PORT}`);
 });
+
+app.use("/api/users", userRoutes);
